19dfd556aee8ebd2a779280d769850c27c0baef3
halodong <drl990114@gmail.com>
2021-12-07 22:53:22 +0800

    chore: createElement

--- src/react.js
+++ src/react.js
@@ -1,9 +1,29 @@
-function createElement () {
-
+import { REACT_ELEMENT } from "./constants";
+function createElement(type, config, children) {
+  const props = { ...config };
+  let ref, key;
+  if (config) {
+    ref = config.ref || null;
+    key = config.key || null;
+    delete config.ref;
+    delete config.key;
+  }
+  if (arguments.length > 3) {
+    props.children = Array.prototype.slice.call(arguments, 2);
+  } else if (arguments.length === 3) {
+    props.children = children;
+  }
+  return {
+    $$typeof: REACT_ELEMENT,
+    props,
+    type,
+    ref,
+    key,
+  };
 }
 
 const React = {
-    createElement
-}
+  createElement,
+};
 
-export default React
+export default React;
