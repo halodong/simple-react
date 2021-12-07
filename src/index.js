014d7aaedbb929fcaeb0d12c974ff49581017c5b
halodong <drl990114@gmail.com>
2021-12-07 22:54:22 +0800

    chore:  add compared

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+import myReact from "./react";
+import myReactDOM from "./react-dom";
+const ReactEl = React.createElement("h1", { id: "title" }, "hello");
+const ReactEl1 = myReact.createElement("h1", { id: "title" }, "hello");
+console.log(ReactEl, ReactEl1);
+ReactDOM.render(ReactEl, document.getElementById("root"));
+myReactDOM.render(ReactEl1, document.getElementById("root"));
